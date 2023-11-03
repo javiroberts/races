77d45c950efa87bc7e3635f0465b442586364497
Javi Roberts <jrobertsotero@gmail.com>
2023-11-02 23:22:46 -0300

    base structure ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen bg-gradient-to-br from-purple-400 to-yellow-400">
+    <h1 class="text-center text-slate-100">
+        Proximas carreras
+    </h1>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+        overflow: hidden;
+        height: 100%;
+    }
+</style>
