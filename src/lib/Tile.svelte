58f7b3af315257c1abeb80df0725a236322b69a9
Javi Roberts <jrobertsotero@gmail.com>
2023-11-03 00:12:19 -0300

    adding tile element

---
+++ src/lib/Tile.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let title: String;
+    export let link: String;
+    export let imageSrc: String = undefined;
+    export let likeCount: Number;
+</script>
+
+<div class="border-solid border-2 border-neutral-50 rounded break-inside-avoid">
+    <a href="${link}"><h2>{title}</h2></a>
+    <p>{likeCount}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
-<div class="h-screen bg-gradient-to-br from-purple-400 to-yellow-400">
-    <h1 class="text-center text-slate-100">
-        Proximas carreras
+<script>
+    import Tile from "$lib/tile.svelte";
+</script>
+
+<div class="h-screen bg-gradient-to-br from-purple-400 to-yellow-400 p-6">
+    <h1 class="text-center text-5xl font-black text-neutral-50 underline decoration-4 decoration-yellow-400">
+        Próximas Carreras
     </h1>
+
+    <div class="columns-5 mx-32 mt-10">
+        <Tile title="test-race" link="#" likeCount={12} />
+        <Tile title="test-race" link="#" likeCount={11} />
+        <Tile title="test-race" link="#" likeCount={10} />
+        <Tile title="test-race" link="#" likeCount={9} />
+        <Tile title="test-race" link="#" likeCount={8} />
+        <Tile title="test-race" link="#" likeCount={7} />
+        <Tile title="test-race" link="#" likeCount={6} />
+        <Tile title="test-race" link="#" likeCount={5} />
+        <Tile title="test-race" link="#" likeCount={4} />
+    </div>
+    
 </div>
 
 <style lang="postcss">
